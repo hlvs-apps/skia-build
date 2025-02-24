ca7278ee6b256e69cc9af8c49fb6866e910a3225
Henri Sauer <henri@sauers.de>
2025-02-24 10:39:33 +0100

    update python

--- script/checkout.py
+++ script/checkout.py
@@ -48,7 +48,7 @@ def main():
     env['PYTHONHTTPSVERIFY']='0'
     subprocess.check_call(["python", "tools/git-sync-deps"], env=env)
   else:
-    subprocess.check_call(["python2", "tools/git-sync-deps"])
+    subprocess.check_call(["python3", "tools/git-sync-deps"])
 
   return 0
 
